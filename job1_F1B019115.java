164a6c32d3ceb7d77e3e91decf2ec095a68b94d0
RaditiaAstarina <148021980+RaditiaAstarina@users.noreply.github.com>
2023-10-25 19:24:57 +0800

    Create job1_F1B019115.java

---
+++ job1_F1B019115.java
@@ -0,0 +1,13 @@
+package jsp3;
+
+public class jobsheet1 { 
+    String x ; 
+
+public jobsheet1() {
+}
+
+public static void main(String[] args){ jobsheet1 mhs = new jobsheet1();
+mhs.x="Raditia Astarina";
+System.out.println(mhs.x);
+}
+}
