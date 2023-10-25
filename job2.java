ce4290e28adca00bd33d8316d11d617c8e9d154b
Macchyy <148258930+Macchyy@users.noreply.github.com>
2023-10-25 15:21:37 +0800

    Add files via upload

---
+++ job2.java
@@ -0,0 +1,20 @@
+
+package P2;
+
+
+public class job2 { 
+    String x; 
+
+public job2( ) {
+    
+}
+public job2(String nma) {
+    x=nma;
+}
+
+public static void main(String[] args){ 
+job2 mhs = new job2("M. rifqi");
+
+System.out.println(mhs.x);
+}
+}
