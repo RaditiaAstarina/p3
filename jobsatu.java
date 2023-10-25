3491bd39daafe3e26cc97c920658b67ff6cf1c2b
Macchyy <148258930+Macchyy@users.noreply.github.com>
2023-10-25 15:03:09 +0800

    Add files via upload

---
+++ jobsatu.java
@@ -0,0 +1,15 @@
+
+package newpackage1;
+
+public class jobsatu { 
+    int x; 
+
+public jobsatu() {
+}
+
+public static void main(String[] args){ jobsatu mhs = new jobsatu();
+
+System.out.println(mhs.x);
+}
+}
+
